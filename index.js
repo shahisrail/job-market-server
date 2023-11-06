a5d404fb275632db14ed10bbcba15af8a2b56786
shah israil <shahisrail134@gmail.com>
2023-11-06 21:40:15 +0600

    a job delete functoinality done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// middalwre
+// Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -17,7 +17,7 @@ const client = new MongoClient(uri, {
   }
 });
 
-let Addjobs; // Declare the Addjobs variable at a higher scope
+let Addjobs;
 
 async function run() {
   try {
@@ -26,9 +26,8 @@ async function run() {
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+  } catch (error) {
+    console.error(error);
   }
 }
 
@@ -45,9 +44,19 @@ app.post('/cart', async (req, res) => {
 });
 
 app.get('/cart', async (req, res) => {
-  const cursor = Addjobs.find();
-  const result = await cursor.toArray();
-  res.send(result);
+  try {
+    const query = {};
+
+    if (req.query.Email) {
+      query.Email = req.query.Email;
+    }
+
+    const result = await Addjobs.find(query).toArray();
+    res.send(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 app.get('/cart/:id', async (req, res) => {
@@ -56,16 +65,33 @@ app.get('/cart/:id', async (req, res) => {
   const result = await Addjobs.findOne(query);
   res.send(result);
 });
-app.delete('/cart/:id', async (req, res) => {
-  const id = req.params.id
-  const query = { _id: new ObjectId(id)  }
-  const result = await Addjobs.deleteOne(query)
-  res.send(result)
 
-})
+app.put('/cart/:id', async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+  const updatedCart = req.body;
+  const Cart = {
+    $set: {
+      Email: updatedCart.Email,
+      Jobtitle: updatedCart.Jobtitle,
+      Deadline: updatedCart.Deadline,
+      Description: updatedCart.Description,
+      Category: updatedCart.Category,
+      Minimumprice: updatedCart.Minimumprice,
+      Maximumprice: updatedCart.Maximumprice
+    }
+  };
+  const result = await Addjobs.updateOne(filter, Cart);
+  res.send(result);
+});
+
+app.delete('/cart/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await Addjobs.deleteOne(query);
+  res.send(result);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-
