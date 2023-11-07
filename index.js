e4b38e859c50e857af5f811e53eff9b529535774
shah israil <shahisrail134@gmail.com>
2023-11-08 02:44:36 +0600

    spesific user data post

--- index.js
+++ index.js
@@ -18,11 +18,14 @@ const client = new MongoClient(uri, {
 });
 
 let Addjobs;
+let mybids;
 
 async function run() {
   try {
     await client.connect();
+
     Addjobs = client.db("Jobmarket").collection('Addjobs');
+    mybids = client.db("Jobmarket").collection('mybids');
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -36,7 +39,7 @@ run().catch(console.dir);
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-
+// post
 app.post('/cart', async (req, res) => {
   const carts = req.body;
   const result = await Addjobs.insertOne(carts);
@@ -46,11 +49,9 @@ app.post('/cart', async (req, res) => {
 app.get('/cart', async (req, res) => {
   try {
     const query = {};
-
     if (req.query.Email) {
       query.Email = req.query.Email;
     }
-
     const result = await Addjobs.find(query).toArray();
     res.send(result);
   } catch (error) {
@@ -91,6 +92,98 @@ app.delete('/cart/:id', async (req, res) => {
   const result = await Addjobs.deleteOne(query);
   res.send(result);
 });
+
+
+// my bits 
+app.post('/mybids', async (req, res) => {
+  const carts = req.body;
+  const result = await mybids.insertOne(carts);
+  res.send(result);
+});
+
+app.get('/mybids', async (req, res) => {
+  try {
+    const query = {};
+    if (req.query.Email) {
+      query.Email = req.query.Email;
+    }
+    const result = await mybids.find(query).toArray();
+    res.send(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get('/mybids/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await mybids.findOne(query);
+  res.send(result);
+});
+
+
+// byer bids request
+app.post('/BidRequest', async (req, res) => {
+  const carts = req.body;
+  const result = await mybids.insertOne(carts);
+  res.send(result);
+});
+
+app.get('/BidRequest', async (req, res) => {
+  try {
+    const query = {};
+    if (req.query.Byeremail) {
+      query.Byeremail = req.query.Byeremail;
+    }
+    const result = await mybids.find(query).toArray();
+    res.send(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+
+// byer delete and ackcept 
+
+app.patch('/BidRequest/:id', async(request, response) => {
+  const id = request.params.id
+  const query = { _id: new ObjectId(id) }
+
+  const updateStatue = {
+    $set: {
+     status: request.body.status,
+    },
+  };
+  const result = await mybids.updateOne(query, updateStatue);
+  response.send(result);
+});
+app.patch('/mybids/:id', async(request, response) => {
+  const id = request.params.id
+  const query = { _id: new ObjectId(id) }
+
+  const updateStatue = {
+    $set: {
+     status: request.body.status,
+    },
+  };
+  const result = await mybids.updateOne(query, updateStatue);
+  response.send(result);
+});
+app.patch('/BidRequest/:id', async(request, response) => {
+  const id = request.params.id
+  const query = { _id: new ObjectId(id) }
+
+  const updateStatue = {
+    $set: {
+     status: request.body.status,
+    },
+  };
+  const result = await mybids.updateOne(query, updateStatue);
+  response.send(result);
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
